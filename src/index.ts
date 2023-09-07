2f56e13cb6c04927a84344b5a08bdc1187f9efdc
Luke Shiru <hi@luke.sh>
2023-09-07 18:45:25 -0500

    🎉 initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export type { EventListener } from "./EventListener.js";
+export type { EventRegistry } from "./EventRegistry.js";
+export type { EventTypeDictionary } from "./EventTypeDictionary.js";
+export { broadcast } from "./broadcast.js";
+export { emit } from "./emit.js";
+export { on } from "./on.js";
